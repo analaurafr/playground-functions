6b7362d609c2ebcec7a5345f3e5ec399435e2535
analaurafr <laurafernandes@outlook.com>
2023-05-17 01:07:08 -0300

    quase acabando as resoluções

--- src/challenges.js
+++ src/challenges.js
@@ -79,7 +79,79 @@ function catAndMouse(mouse, cat1, cat2) {
 
 // Desafio 8 - Crie a função fizzBuzz
 
+const fizzBuzz = (array) => {
+  let arrayFizzBuzz = [];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] % 3 === 0 && array[index] % 5 === 0) {
+      arrayFizzBuzz.push('fizzBuzz');
+    } else if (array[index] % 5 === 0) {
+      arrayFizzBuzz.push('buzz');
+    } else if (array[index] % 3 === 0) {
+      arrayFizzBuzz.push('fizz');
+    } else {
+      arrayFizzBuzz.push('bug!');
+    }
+  }
+  return arrayFizzBuzz;
+};
+
 // Desafio 9 - Crie a função encode e a função decode
+
+function encode(string) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  let encodedString = '';
+
+  for (let i = 0; i < string.length; i++) {
+    const character = string[i].toLowerCase();
+
+    if (vowels.includes(character)) {
+      if (character === 'a') {
+        encodedString += '1';
+      } else if (character === 'e') {
+        encodedString += '2';
+      } else if (character === 'i') {
+        encodedString += '3';
+      } else if (character === 'o') {
+        encodedString += '4';
+      } else if (character === 'u') {
+        encodedString += '5';
+      }
+    } else {
+      encodedString += string[i];
+    }
+  }
+
+  return encodedString;
+}
+
+function decode(string) {
+  const numbers = ['1', '2', '3', '4', '5'];
+
+  let decodedString = '';
+
+  for (let i = 0; i < string.length; i++) {
+    const character = string[i];
+
+    if (numbers.includes(character)) {
+      if (character === '1') {
+        decodedString += 'a';
+      } else if (character === '2') {
+        decodedString += 'e';
+      } else if (character === '3') {
+        decodedString += 'i';
+      } else if (character === '4') {
+        decodedString += 'o';
+      } else if (character === '5') {
+        decodedString += 'u';
+      }
+    } else {
+      decodedString += string[i];
+    }
+  }
+
+  return decodedString;
+}
 
 // Desafio 10 - Crie a função techList
 
